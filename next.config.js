48ebfaa088acc9d3f3f88aec98cb5eaab0bd5b1b
= <=>
2023-04-24 06:19:44 +0530

    removed env variables

--- next.config.js
+++ next.config.js
@@ -2,12 +2,7 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-  },
-  env: {
-    SUPABASE_SECRETE_KEY: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-        .eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRmeXJlZGpkdW5zd3hreG16eXdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIyNTY5ODcsImV4cCI6MTk5NzgzMjk4N30
-        .Kn3L9_7YYmCJKfZpOVgLYnasMcTxGOKWZVLTNgSrkAo`,
-  },
+  }
 };
 
 module.exports = nextConfig;
