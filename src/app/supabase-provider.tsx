8a55e03b12916c0a36a6c03ed461ef0e1feb686f
= <=>
2023-04-24 11:33:22 +0530

    fixes authorization data

--- src/app/supabase-provider.tsx
+++ src/app/supabase-provider.tsx
@@ -28,11 +28,22 @@ export default function SupabaseProvider({
   } | null>(null);
   const router = useRouter();
 
+  async function getUserData() {
+    const { data } = await supabase.auth.getSession();
+    const { data: User } = await supabase
+      .from("profiles")
+      .select("username")
+      .eq("id", data.session?.user.id)
+      .single();
+    setUser({ username: User?.username, email: data.session?.user.email });
+  }
+
   useEffect(() => {
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange(() => {
       router.refresh();
+      getUserData()
     });
 
     return () => {
@@ -41,18 +52,10 @@ export default function SupabaseProvider({
   }, [router, supabase]);
 
   useEffect(() => {
-    async function getUserData() {
-      const { data } = await supabase.auth.getSession();
-      const { data: User } = await supabase
-        .from("profiles")
-        .select("username")
-        .eq("id", data.session?.user.id)
-        .single();
-      setUser({ username: User?.username, email: data.session?.user.email });
-    }
     getUserData();
   }, [router, supabase]);
 
+  
   return (
     <Context.Provider value={{ supabase, user }}>
       <>{children}</>
