10b27f5c03987d0f94dfa50f5cc6c3c6dd2cbc92
= <=>
2023-07-28 15:41:17 +0530

    add college

--- src/app/(dashboard)/students/layout.tsx
+++ src/app/(dashboard)/students/layout.tsx
@@ -2,13 +2,15 @@
 import { fetchBranchList } from "@/store/fees.slice";
 import "../../globals.css";
 import { store } from "@/store";
+import { useSupabase } from "@/app/supabase-provider";
 
 export default function DashboardRootLayout(props: {
   children: React.ReactNode;
 }) {
   const branchList = store.getState().fees.all_fee.data;
+  const user = useSupabase().user
 
-  branchList.length == 0 && store.dispatch(fetchBranchList());
+  branchList.length == 0 && store.dispatch(fetchBranchList({college:user?.college!}));
 
   return (
     <>
