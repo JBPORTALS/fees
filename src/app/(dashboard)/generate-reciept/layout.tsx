c498e163071866e7a3dc128ef53b366b85a23b4b
= <=>
2023-07-28 15:38:51 +0530

    add college

--- src/app/(dashboard)/generate-reciept/layout.tsx
+++ src/app/(dashboard)/generate-reciept/layout.tsx
@@ -4,13 +4,15 @@ import GenerateRecieptLayout from "@/components/layouts/GenerateRecieptLayout";
 import { store } from "../../../store/";
 import { useCallback, useEffect } from "react";
 import { fetchBranchList } from "@/store/fees.slice";
+import { useSupabase } from "@/app/supabase-provider";
 
 export default async function GenerateRecieptRootLayout(props: {
   children: React.ReactNode;
 }) {
+  const user = useSupabase().user;
 
   const fetchBranchListCb = useCallback(()=>{
-      store.dispatch(fetchBranchList())
+      store.dispatch(fetchBranchList({college:user?.college!}))
   },[store.dispatch]);
  
   useEffect(()=>{
