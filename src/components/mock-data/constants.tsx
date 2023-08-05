da2d3f1035170c84766a5c9faa04f5be8a808169
= <=>
2023-08-05 12:18:59 +0530

    add colleges

--- src/components/mock-data/constants.tsx
+++ src/components/mock-data/constants.tsx
@@ -1,21 +1,75 @@
-export const BANKS = [
-  {
-    option: "UNION",
-    value: "UNION",
-  },
-  {
-    option: "AXIS",
-    value: "AXIS",
-  },
-  {
-    option: "ICICI",
-    value: "ICICI",
-  },
-  {
-    option:"OFFICE",
-    value:"OFFICE"
-  }
-];
+export const BANKS = (college: string | undefined) => {
+  if (college == "KSPU")
+    return [
+      {
+        option: "ICICI",
+        value: "ICICI",
+      },
+    ];
+  else if (college == "KSPT")
+    return [
+      {
+        option: "UNION",
+        value: "UNION",
+      },
+      {
+        option: "AXIS",
+        value: "AXIS",
+      },
+      {
+        option: "OFFICE",
+        value: "OFFICE",
+      },
+    ];
+  else if (college == "KSSA")
+    return [
+      {
+        option: "AXIS",
+        value: "AXIS",
+      },
+    ];
+  else if (college == "KSSEM")
+    return [
+      {
+        option: "AXIS",
+        value: "AXIS",
+      },
+      {
+        option: "UPI SCAN",
+        value: "UPI SCAN",
+      },
+    ];
+  else if (college == "KSIT" || college == "KSSEM" || college == "KSSA")
+    return [
+      {
+        option: "AXIS",
+        value: "AXIS",
+      },
+      {
+        option: "UNION",
+        value: "UNION",
+      },
+    ];
+  else
+    return [
+      {
+        option: "UNION",
+        value: "UNION",
+      },
+      {
+        option: "AXIS",
+        value: "AXIS",
+      },
+      {
+        option: "ICICI",
+        value: "ICICI",
+      },
+      {
+        option: "OFFICE",
+        value: "OFFICE",
+      },
+    ];
+};
 
 export const CATS = [
   {
