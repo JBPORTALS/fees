2032ba1cd713d0b839e090ce2453582a33b6c14c
JB PORTALS <118927681+JBPORTALS@users.noreply.github.com>
2025-03-18 12:14:58 +0530

    Added 2025 option to the SideBar and updated local development URL in trpc-provider

--- src/components/ui/SideBar.tsx
+++ src/components/ui/SideBar.tsx
@@ -97,6 +97,7 @@ export default function SideBar() {
               router.refresh();
             }}
           >
+            <option value={"2025"}>2025</option>
             <option value={"2024"}>2024</option>
             <option value={"2023"}>2023</option>
           </Select>
